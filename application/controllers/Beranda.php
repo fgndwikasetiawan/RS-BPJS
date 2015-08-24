d01b96df099e11c2d40f144417e45efe8f34e460
agus4171 <agus.4171.wirawan@gmail.com>
2015-08-24 10:41:46 +0700

    tambah beranda

---
+++ application/controllers/Beranda.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Beranda extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		if (!$this->session->has_userdata('username')) {
+			redirect(base_url() . 'Auth');
+		}
+	}
+
+	public function index(){
+		load_main_template('Beranda', 'Sistem Informasi RSUP. Dr. Mohammad Hoesin', 'beranda', null, 0);
+	}
+
+}
---
+++ application/views/beranda.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-md-3">
+    </div>                  
+    <div class="col-md-6">
+    <img src="/assets/images/logo.png" style="width:100%;height:100%;">
+    </div>                  
+	<div class="col-md-3">
+    </div>
+</div>
+
+
